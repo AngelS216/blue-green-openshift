f41123f6a15c177c07cd69ca93d007718f994058
AngelS216 <angel-xi-ting.shih@polymtl.ca>
2025-03-31 19:42:48 -0400

    remove process.env.PORT

--- server.js
+++ server.js
@@ -6,8 +6,6 @@
 var http = require("http");
 var url = require("url");
 
-const port = process.env.PORT || 8080;
-
 function start(route, handle) {
 
     function onRequest(request, response) {
@@ -16,7 +14,7 @@ function start(route, handle) {
         route(handle, pathname, response, request);
     }
 
-    http.createServer(onRequest).listen(port);
+    http.createServer(onRequest).listen(8080);
 }
 
 exports.start = start;
